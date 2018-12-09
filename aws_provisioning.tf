015dd27447121e476256ed0f71b073df669d8422
masatomo-ito2 <45160975+masatomo-ito2@users.noreply.github.com>
2018-12-09 16:47:59 +0900

    Add files via upload

---
+++ aws_provisioning.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+	organization = "masa_org"
+
+	workspaces {
+		prefix = "jenkins_pipeline_"
+	}	
+  }	
+}
+
+provider "aws" {
+  region     = "ap-northeast-1"
+}
+
+resource "aws_instance" "masa_tfe" {
+  ami           = "ami-08847abae18baa040"
+  instance_type = "t2.micro"
+  count			= "${var.node_count}"
+
+  # My security setting
+  security_groups = ["${aws_security_group.default.name}"]
+  tags {
+  	Name = "${var.env_name}"
+  }
+}
+
+resource "aws_security_group" "jenkins_security_group" {
+  name        = "security group for ${var.env_name}"
+  description = "Allow access to SSH port"
+}
+
